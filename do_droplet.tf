8dfaea3fc1611abb3cba41b790cc71701d15d5a6
Matteo Ruina <matteo.ruina@gmail.com>
2016-04-02 23:56:04 +0100

    do_droplet: Change OS to Debian 7

--- do_droplet.tf
+++ do_droplet.tf
@@ -5,8 +5,8 @@ resource "digitalocean_ssh_key" "tavore_root_key" {
 }
 
 resource "digitalocean_droplet" "tavore" {
-    image = "ubuntu-14-04-x64"
-    name = "web-1"
+    image = "debian-7-0-x64"
+    name = "tavore"
     region = "lon1"
     size = "512mb"
     backups = true
